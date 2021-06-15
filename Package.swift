29c53ce6156a1ee51aa41fdc79fb047e4c758892
Shane Whitehead <shanew@swann.com>
2021-06-15 20:43:31 +1000

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -16,10 +16,11 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-			.package(url: "https://github.com/RustyKnight/HttpEngineCore", Package.Dependency.Requirement.branch("feature/spm")),
-			.package(url: "https://github.com/Alamofire/Alamofire", "4.8.2"..."4.8.2"),
+			.package(url: "https://github.com/RustyKnight/HttpEngineCore", Package.Dependency.Requirement.branch("feature/pk-xcframeworks")),
+			.package(url: "https://github.com/Alamofire/Alamofire", .exact("4.8.2")),
 			.package(url: "https://github.com/malcommac/Hydra.git", from: "2.0.0"),
 			.package(url: "https://github.com/RustyKnight/Cadmus", Package.Dependency.Requirement.branch("feature/spm")),
+            .package(url: "https://github.com/mxcl/PromiseKit", .exact("6.14.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -29,7 +30,7 @@ let package = Package(
             dependencies: [
 							"HttpEngineCore",
 							"Alamofire",
-							"Hydra",
+                            .product(name: "PromiseKit", package: "PromiseKit"),
 							"Cadmus",
 				],
 						path: "AlamofireHttpEngineKit")
